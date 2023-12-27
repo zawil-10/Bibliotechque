9a665726a4db58147e522b6d016780ee056c206b
new_projet <wilfriedzanmo@gmail.com>
2023-12-27 13:51:10 +0100

    Implemantation des tests unitaires

---
+++ orite_objet_unitaire.py
@@ -0,0 +1,50 @@
+import unittest
+from orite_objet import *
+
+
+class TestUtilisateur(unittest.TestCase):
+    def test_emprunter_exemplaire(self):
+        utilisateur = Utilisateur("Doe", "John", "john@example.com")
+        exemplaire = Exemplaire(identifiant=1)
+        utilisateur.emprunter(exemplaire)
+        self.assertIn(exemplaire, utilisateur.emprunts)
+        self.assertFalse(exemplaire.disponible)
+
+    def test_retourner_exemplaire(self):
+        utilisateur = Utilisateur("Doe", "John", "john@example.com")
+        exemplaire = Exemplaire(identifiant=1)
+        utilisateur.emprunter(exemplaire)
+        utilisateur.retourner(exemplaire)
+        self.assertNotIn(exemplaire, utilisateur.emprunts)
+        self.assertTrue(exemplaire.disponible)
+
+
+class TestExemplaire(unittest.TestCase):
+    def test_set_disponibilite(self):
+        exemplaire = Exemplaire(identifiant=1)
+        exemplaire.set_disponibilite(False)
+        self.assertFalse(exemplaire.disponible)
+
+
+class TestEmplacement(unittest.TestCase):
+    def test_assigner_exemplaire(self):
+        emplacement = Emplacement(rayon="A", etagere=1)
+        exemplaire = Exemplaire(identifiant=1)
+        emplacement.assigner_exemplaire(exemplaire)
+        self.assertEqual(exemplaire.emplacement, emplacement)
+
+
+class TestLivre(unittest.TestCase):
+    def test_obtenir_ISBN(self):
+        livre = Livre("Le Seigneur des Anneaux", "J.R.R. Tolkien", 1954, "1234567890")
+        self.assertEqual(livre.obtenir_ISBN(), "1234567890")
+
+
+class TestRevue(unittest.TestCase):
+    def test_obtenir_numero(self):
+        revue = Revue("Titre de la Revue", "Auteur de la Revue", 2023, 5)
+        self.assertEqual(revue.obtenir_numero(), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
